5b5bd521e0723d9f3c532ef6dd25654d0d70f3ed
Tommaso Soru <mommi84@gmail.com>
2016-06-10 19:13:17 +0200

    getting entity/property IDs on-the-fly

--- test/rdf_stream_sqlite.py
+++ test/rdf_stream_sqlite.py
@@ -22,17 +22,24 @@ class StreamSink(Sink):
         # print "Stream of triples s={s}, p={p}, o={o}".format(s=s, p=p, o=o).encode('utf8')
         try:
             c.execute("INSERT INTO entities (entity) VALUES (?)", [(s)])
+            s_id = c.last_insert_rowid()
         except sqlite3.IntegrityError:
-            pass
+            c.execute("SELECT id FROM entities WHERE entity = ?", [(s)])
+            s_id = c.fetchone()[0]
         try:    
             c.execute("INSERT INTO properties (property) VALUES (?)", [(p)])
+            p_id = c.last_insert_rowid()
         except sqlite3.IntegrityError:
-            pass
+            c.execute("SELECT id FROM properties WHERE property = ?", [(p)])
+            p_id = c.fetchone()[0]
         try:
             c.execute("INSERT INTO entities (entity) VALUES (?)", [(o)])
+            o_id = c.last_insert_rowid()
         except sqlite3.IntegrityError:
-            pass
-
+            c.execute("SELECT id FROM entities WHERE entity = ?", [(o)])
+            o_id = c.fetchone()[0]
+        
+        # print "{} {} {}".format(s_id, p_id, o_id)
 
 if __name__ == "__main__":
     # Create a new parser and try to parse the NT file.
